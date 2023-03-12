9f0c503e6c064f665369b2245dcc44178ad42729
JassonArdila <ja_84@outlook.com>
2023-03-11 19:58:28 -0500

    tipos primitivos en java
    integer,numericos, flotantes, interfaces tipo var, tipos primitivos char, boolean y  conversiones.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-     /*
-        tipos primitivos enteros: byte, short, int, long
-        */
-     byte NumeroByte=10;
-        System.out.println("Valor minimo byte" + Byte.MIN_VALUE);
-        System.out.println("Valor maximo byte" + Byte.MAX_VALUE);
+        //convertir un tipo string a un tipo int
+        var edad = Integer.parseInt("20");
+        System.out.println("edad =" + (edad + 1));
+      var valorPi= Double.parseDouble("3.1416");
+        System.out.println("valorPi = " + valorPi);
+        
+        //pedir un valor.
+//        var consola = new Scanner(System.in);
+//        System.out.println("ingresa tu edad");
+//        edad = Integer.parseInt(consola.nextLine());
+//        System.out.println("edad = " + edad);
+        
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+        
+      var caracter = "hola".charAt(2);
+        System.out.println("caracter = " + caracter);
     }
 }
